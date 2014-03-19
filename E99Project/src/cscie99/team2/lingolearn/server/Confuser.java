f079045793b22728ff629508d55f965c6d0442f3
rjzupkoii <rjzii@yahoo.com>
2014-03-18 22:18:33 -0400

    #12 Updated the code to check what type of character a given character is.

--- E99Project/src/cscie99/team2/lingolearn/server/Confuser.java
+++ E99Project/src/cscie99/team2/lingolearn/server/Confuser.java
@@ -2,6 +2,10 @@ package cscie99.team2.lingolearn.server;
 
 import cscie99.team2.lingolearn.shared.error.ConfuserException;
 
+/**
+ * This class encapsulates a means of getting characters that are similar to 
+ * a given example for the purposes of confusing humans.
+ */
 public class Confuser {
 	/**
 	 * The various character types that are recognized by the confuser algorithm
@@ -14,24 +18,31 @@ public class Confuser {
 	}
 	
 	/**
+	 * Check to see what type of Japanese character the given character is.
+	 * Note that this code does not detect if a kanji is limited ot use in 
+	 * Chinese as opposed to Japanese.
 	 * 
-	 * @param character
-	 * @return
-	 * @throws ConfuserException 
+	 * @param character The character to be tested.
+	 * @return The type of character provided, or unknown if it was not determined.
 	 */
 	public static CharacterType checkCharacter(String character) throws ConfuserException {
 		// Make sure we are only working with a single character
 		if (character.length() != 1) {
 			throw new ConfuserException("The character string is an invalid length.");
 		}		
-
-		// TODO Check to see if this is a hiragana
-		
-		// TODO Check to see if this is a katakana
-		
-		// TODO Check to see if this is kanji
-
-
+		// Check to see if this is a hiragana
+		char ch = character.charAt(0);
+		if (ch >= 0x3041 && ch <= 0x3096) {
+			return CharacterType.Hiragana;
+		}
+		// Check to see if this is a katakana
+		if (ch >= 0x30A1 && ch <= 0x30FC) {
+			return CharacterType.Katakana;
+		}
+		// Check to see if this is kanji
+		if (ch >= 0x4E00 && ch <= 0x9FAF) {
+			return CharacterType.Kanji;
+		}
 		// Fall through to unknown
 		return CharacterType.Unknown;
 	}
